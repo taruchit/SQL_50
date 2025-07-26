5280674c226f40c2dc2774a346b8d0e23fc23c4e
Taruchit Goyal <taruchit.goyal@gmail.com>
2025-07-26 06:46:55 +0530

    Time: 439 ms (84.62%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,21 +1,4 @@
 # Write your MySQL query statement below
-/*
-SELECT product_id, new_price, MAX(change_date) AS max_change_date
-FROM
-(SELECT product_id, new_price, change_date
-FROM Products
-WHERE change_date<="2019-08-16"
-GROUP BY product_id, change_date
-ORDER BY change_date DESC, product_id) AS dervived_table
-GROUP BY product_id
-*/
-/*
-SELECT DISTINCT a.product_id, b.product_id, b.new_price, b.change_date
-FROM Products a
-LEFT JOIN Products b
-ON a.product_id=b.product_id
-*/
-
 SELECT product_id, new_price AS price
 FROM Products
 WHERE (product_id, change_date) IN 
