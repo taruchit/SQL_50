0701be8bc10e31a0663114aa82df845176fbb85e
Taruchit Goyal <taruchit.goyal@gmail.com>
2025-07-30 09:51:22 +0530

    Time: 326 ms (77.84%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT
+CASE
+WHEN id=(SELECT MAX(id) FROM Seat) AND id%2=1 THEN id
+WHEN id%2=0 THEN id-1
+WHEN id%2=1 THEN id+1
+END AS id, student
+FROM Seat
+ORDER BY id ASC
